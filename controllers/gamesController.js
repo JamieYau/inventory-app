cd19793e8bab057e3d1bff929a79d1ff3011d8c0
JamieYau <jamie.yau2002@me.com>
2024-01-17 21:47:44 +0000

    Add create route and controller for games

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -8,3 +8,9 @@ exports.index = asyncHandler(async function (req, res, next) {
   const categories = await Category.find(); // Retrieve all categories from the database
   res.render("games", { title: "All Games" , games, categories}); // Render the 'games/index' view, passing the games data
 });
+
+exports.create = asyncHandler(async function (req, res, next) {
+  const newGame = new Game(req.body); // Create a new Game document with the data from the request body
+  await newGame.save(); // Save the new Game document to the database
+  res.redirect("/games"); // Redirect the user to the games list
+});
--- routes/games.js
+++ routes/games.js
@@ -3,9 +3,8 @@ const express = require("express");
 const router = express.Router();
 const gamesController = require("../controllers/gamesController");
 
-router.get("/", gamesController.index); // Show all items
-// router.get("/create", itemsController.showCreateForm); // Show the form to create a new item
-// router.post("/", itemsController.create); // Save a new item
+router.get("/", gamesController.index); // Show all games
+router.post("/", gamesController.create); // Save a new game
 // router.get("/:id", itemsController.show); // Show a single item
 // router.get("/:id/edit", itemsController.showEditForm); // Show the form to edit an existing item
 // router.put("/:id", itemsController.update);
