72ea8b84cad90c84b8f965b37c4820ff1f8ada18
JamieYau <jamie.yau2002@me.com>
2024-01-17 00:01:14 +0000

    Add category display to games view
    This commit adds the display of the game category to the games view. refactored gamecontroller.index to retrieve category name

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 const Game = require("../models/game");
 
 exports.index = asyncHandler(async function (req, res, next) {
-  const games = await Game.find(); // Retrieve all games from the database
+  const games = await Game.find().populate("category"); // Retrieve all games from the database
   console.log(games);
   res.render("games", { title: "All Games" , games}); // Render the 'games/index' view, passing the games data
 });
