18910fc87e6ab54eea01cf5997e14d2a7f9a2cd8
JamieYau <jamie.yau2002@me.com>
2024-01-18 06:09:12 +0000

    add game modals

--- public/javascripts/games.js
+++ public/javascripts/games.js
@@ -1,17 +1,51 @@
-const newGameButton = document.getElementById("new-game-button");
+const createGameButton = document.getElementById("new-game-button");
+const editGameButtons = document.querySelectorAll(".edit-button");
+const deleteGameButtons = document.querySelectorAll(".delete-button");
 const overlay = document.querySelector(".overlay");
-const modal = document.getElementById("new-game");
+const modals = document.querySelectorAll(".modal");
+const createModal = document.getElementById("new-game");
+const editModal = document.getElementById("edit-game");
+const deleteModal = document.getElementById("delete-game");
 
-const openModal = () => {
+modals.forEach((modal) => {
+  modal.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+});
+const openModal = (modal) => {
   overlay.style.display = "block";
+  modal.style.display = "block";
 };
-
-const closeModal = () => {
+const closeModals = () => {
   overlay.style.display = "none";
+  modals.forEach((modal) => {
+    modal.style.display = "none";
+  });
 };
+overlay.addEventListener("click", closeModals);
 
-newGameButton.addEventListener("click", openModal);
-overlay.addEventListener("click", closeModal);
-modal.addEventListener("click", (event) => {
-  event.stopPropagation();
+createGameButton.addEventListener("click", () => openModal(createModal));
+
+editGameButtons.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    const gameId = this.closest(".game").dataset.gameId;
+    const editForm = document.getElementById("edit-form");
+    editForm.action = `/games/edit/${gameId}`;
+    const game = this.closest(".game");
+    const gameName = game.querySelector(".game-name").textContent;
+    const gameDescription = game.querySelector(".game-description")
+      .textContent;
+    editForm.querySelector("#edit-name").value = gameName;
+    editForm.querySelector("#edit-description").value = gameDescription;
+    openModal(editModal);
+  });
+});
+
+deleteGameButtons.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    const gameId = this.closest(".game").dataset.gameId;
+    const deleteForm = document.getElementById("delete-form");
+    deleteForm.action = `/games/delete/${gameId}`;
+    openModal(deleteModal);
+  });
 });
